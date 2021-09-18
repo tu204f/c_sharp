2a1f7f3f9d9201197f0f7c83c6027e26fbb77d42
tu204f <david_yusupov@mail.ru>
2021-09-18 21:48:14 +0300

    Быстрая сортировака

---
+++ sample/sort/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace sort
+{
+    class Program
+    {
+
+        static int[] array = { 10, 98, 78, 4, 54, 25, 84, 41, 30, 87, 6 };
+
+        static void Main(string[] args)
+        {
+            sort(0, array.Length - 1);
+            foreach (int i in array)
+                Console.WriteLine(i);
+        }
+
+        static void sort(int l, int r)
+        {
+            int i = l;
+            int j = r;
+            int x = array[(l + r) / 2];
+
+            do
+            {
+                while (array[i] < x) i++;
+                while (array[j] > x) j--;
+                if (i <= j)
+                {
+                    int y = array[i];
+                    array[i] = array[j];
+                    array[j] = y;
+                    i++;
+                    j--;
+                }
+            } while (i < j);
+
+            if (l < j)
+                sort(l, j);
+            if (l < r)
+                sort(i, r);
+        }
+    }
+}
