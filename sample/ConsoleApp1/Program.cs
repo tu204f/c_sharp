f5ec8f9bd27a4b27307204d71ace4cb6acfaada1
tu204f <david_yusupov@mail.ru>
2021-09-18 20:19:37 +0300

    Программа на C#

---
+++ sample/ConsoleApp1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        class Shed
+        {
+            int width;
+            int height;
+
+            public int Width
+            {
+                get { return width; }
+                set { width = value; }
+            }
+
+            public int Height
+            {
+                get { return height; }
+                set { height = value; }
+            }
+
+            public int Lengthwise { get; set; }
+
+        }
+
+        static void Main(string[] args)
+        {
+            // Создаем объект
+            Shed myFirstShed = new Shed();
+
+            // Задаем значения свойств
+            myFirstShed.Height = 10;
+            myFirstShed.Width = 20;
+            myFirstShed.Lengthwise = myFirstShed.Width;
+
+            // Вывод на экран значений
+            Console.WriteLine("Высота: " + myFirstShed.Height);
+            Console.WriteLine("Ширина: " + myFirstShed.Width);
+            Console.WriteLine("Глубина: " + myFirstShed.Lengthwise);
+
+        }
+    }
+}
