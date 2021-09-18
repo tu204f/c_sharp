6743eaac07411381ca465a69436d7996e45fc5ab
tu204f <david_yusupov@mail.ru>
2021-09-18 22:04:58 +0300

    Псевдоним для класса

---
+++ sample/EasyCSharp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using output = System.Console;
+
+
+namespace EasyCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           output.ForegroundColor = ConsoleColor.Green;
+            output.WriteLine("Hello World!");
+        }
+    }
+}
