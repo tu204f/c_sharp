c2b5664840f77aca5b853679f04fa64a6cbb53c2
tu204f <david_yusupov@mail.ru>
2021-09-19 11:45:56 +0300

    Работа с объектом

---
+++ sample/PersonClass/Person.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonClass
+{
+    class Person
+    {
+        public Person(string firstrName, string lastName)
+        {
+            this.FirstName = firstrName;
+            this.LastName = lastName;
+        }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public override string ToString()
+        {
+            //return base.ToString();
+            return this.FirstName + " " + LastName;
+        }
+
+
+        public new bool Equals(Object obj)
+        {
+            Person person = (Person)obj;
+            return (this.FirstName == person.FirstName) && (this.LastName == person.LastName);
+        }
+
+    }
+}
---
+++ sample/PersonClass/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace PersonClass
+{
+    class Program
+    {
+
+        static string ComparePerson(Person person1, Person person2)
+        {
+            bool equalParams = person1.Equals(person2);
+            Object personObj = person1;
+            bool fullEqual = personObj.Equals(person2);
+
+            if (fullEqual)
+                return "Абсолютно одинаковые объекты";
+
+            if (equalParams)
+                return "Одинаковые свойства объектов";
+
+            return "Объекты разные";
+        }
+
+        static void Main(string[] args)
+        {
+            Person p = new Person("Михал", "Фленов");
+            Console.WriteLine(p.ToString());
+
+            Object o = p;
+            Console.WriteLine(o.ToString());
+
+
+            Console.WriteLine("****************************************************");
+
+
+            Person p1 = new Person("Юсупов",  "Давид");
+            Person p2 = new Person("Юсупова", "Давида");
+            Person p3 = p1;
+
+            Console.WriteLine(p1.Equals(p2));
+            Console.WriteLine(p1.Equals(p3));
+
+
+            ComparePerson(p1, p2);
+
+
+        }
+    }
+}
