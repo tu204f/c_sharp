175d005e3bc691725fdfb7b6a4dd488922b5c6b4
tu204f <david_yusupov@mail.ru>
2021-09-19 14:24:23 +0300

    Цвет в консоле

--- sample/Configure/Program.cs
+++ sample/Configure/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace Configure
 {
@@ -18,6 +19,20 @@ namespace Configure
             Console.WriteLine("Признавайся, где народ");
 
             Console.ForegroundColor = ConsoleColor.White;
+
+
+            ConsoleColor[] colors = { ConsoleColor.Blue, ConsoleColor.Red, ConsoleColor.White, ConsoleColor.Yellow };
+            foreach (ConsoleColor color in colors)
+            {
+                //Console.CursorLeft = (Console.BufferWidth - color.ToString().Length / 2);
+                Console.CursorTop = 10;
+                Console.ForegroundColor = color;
+                Console.WriteLine(color);
+                Thread.Sleep(1000);
+                Console.Clear();
+            }
+
+
         }
     }
 }
