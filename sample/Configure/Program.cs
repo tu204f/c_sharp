e747fc2b8ef7b37220b2206cbd867afb5e83d6a9
tu204f <david_yusupov@mail.ru>
2021-09-19 13:35:53 +0300

    ConsoleColor

---
+++ sample/Configure/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Configure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // меняю цвет текста на зеленый
+            Console.ForegroundColor = ConsoleColor.Green;
+            // следующие две строки текста будут зелеными
+            Console.WriteLine("Здравстуй, Давид!");
+            Console.WriteLine("Тебя приветствует матрица!");
+            // меняю цвет текста на красный
+            Console.ForegroundColor = ConsoleColor.Red;
+            // следующие две строки текста будут красными
+            Console.WriteLine("Привет, белый человек");
+            Console.WriteLine("Признавайся, где народ");
+
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
